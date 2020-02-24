6cf0196d53e19bbede1448fd067cb668edd76260
Leonardo Araujo dos Santos <leonardo_a@apple.com>
2020-02-24 11:46:30 +0000

    [RLB-4] Minor refactor

--- crawler_example/crawler_env.py
+++ crawler_example/crawler_env.py
@@ -1,22 +1,35 @@
 """
-This project was developed by Peter Chen, Rocky Duan, Pieter Abbeel for the Berkeley Deep RL Bootcamp, August 2017. Bootcamp website with slides and lecture videos: https://sites.google.com/view/deep-rl-bootcamp/.
+This project was developed by Peter Chen, Rocky Duan, Pieter Abbeel for the Berkeley Deep RL Bootcamp, August 2017.
+Bootcamp website with slides and lecture videos: https://sites.google.com/view/deep-rl-bootcamp/.
 
 Code adapted from CS188 project materials: http://ai.berkeley.edu/project_overview.html.
 
 Copyright 2017 Deep RL Bootcamp Organizers.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
 
 import numpy as np
 from gym import Env, spaces
 import math
-from math import pi as PI
+from math import pi
+import platform
+import subprocess
+import os
 all_envs = []
 
 
@@ -32,7 +45,7 @@ class CrawlingRobotEnv(Env):
             import tkinter
             for env in all_envs:
                 env.close_gui()
-            # all_envs.clear()
+
             all_envs.append(self)
             root = tkinter.Tk()
             root.title('Crawler GUI')
@@ -49,11 +62,10 @@ class CrawlingRobotEnv(Env):
             root.protocol('WM_DELETE_WINDOW', lambda: close)
             root.lift()
             root.attributes('-topmost', True)
-            import platform, subprocess, os
             if platform.system() == 'Darwin':
                 tmpl = 'tell application "System Events" to set frontmost of every process whose unix id is {} to true'
                 script = tmpl.format(os.getpid())
-                output = subprocess.check_call(['/usr/bin/osascript', '-e', script])
+                subprocess.check_call(['/usr/bin/osascript', '-e', script])
             root.attributes('-topmost', False)
         else:
             canvas = None
@@ -62,7 +74,7 @@ class CrawlingRobotEnv(Env):
         robot = CrawlingRobot(canvas)
         self.crawlingRobot = robot
 
-        self._stepCount = 0
+        self._step_count = 0
         self.horizon = horizon
 
         # The state is of the form (armAngle, handAngle)
@@ -105,12 +117,12 @@ class CrawlingRobotEnv(Env):
         return tuple_2_index
 
     @property
-    def stepCount(self):
-        return self._stepCount
+    def step_count(self):
+        return self._step_count
 
-    @stepCount.setter
-    def stepCount(self, val):
-        self._stepCount = val
+    @step_count.setter
+    def step_count(self, val):
+        self._step_count = val
         self.crawlingRobot.draw(val, self.root)
 
     def _legal_actions(self, state):
@@ -123,7 +135,8 @@ class CrawlingRobotEnv(Env):
         actions = list()
 
         curr_arm_bucket, curr_hand_bucket = state
-        if curr_arm_bucket > 0: actions.append(0)
+        if curr_arm_bucket > 0:
+            actions.append(0)
         if curr_arm_bucket < self.nArmStates - 1:
             actions.append(1)
         if curr_hand_bucket > 0:
@@ -138,7 +151,7 @@ class CrawlingRobotEnv(Env):
           Returns:
             s, r, d, info
         """
-        if self.stepCount >= self.horizon:
+        if self.step_count >= self.horizon:
             raise Exception("Horizon reached")
 
         old_x, old_y = self.crawlingRobot.get_robot_position()
@@ -176,11 +189,11 @@ class CrawlingRobotEnv(Env):
             reward *= -1
 
         self.state = next_state
-        self.stepCount += 1
+        self.step_count += 1
 
         state_idx = self.tuple_2_state_idx[tuple(next_state)]
 
-        return state_idx, reward, self.stepCount >= self.horizon, {}
+        return state_idx, reward, self.step_count >= self.horizon, {}
 
     def render(self, mode='human', close=False):
         pass
@@ -195,14 +208,15 @@ class CrawlingRobotEnv(Env):
         self.crawlingRobot.set_angles(self.armBuckets[arm_state], self.handBuckets[hand_state])
         self.crawlingRobot.positions = [20, self.crawlingRobot.get_robot_position()[0]]
 
-        self.stepCount = 0
+        self.step_count = 0
         state_idx = self.tuple_2_state_idx[tuple(self.state)]
         return state_idx
 
 
-
 class CrawlingRobot:
-
+    """
+    Implement drawing function and mechanics of simulated crawler robot
+    """
     def __init__(self, canvas):
 
         # Canvas
@@ -211,21 +225,21 @@ class CrawlingRobot:
         self.lastStep = 0
 
         # Arm and Hand Degrees
-        self.armAngle = self.oldArmDegree = 0.0
-        self.handAngle = self.oldHandDegree = -PI / 6
+        self.arm_angle = self.oldArmDegree = 0.0
+        self.hand_angle = self.oldHandDegree = -pi / 6
 
-        self.maxArmAngle = PI / 6
-        self.minArmAngle = -PI / 6
+        self.maxArmAngle = pi / 6
+        self.minArmAngle = -pi / 6
 
         self.maxHandAngle = 0
-        self.minHandAngle = -(5.0 / 6.0) * PI
+        self.minHandAngle = -(5.0 / 6.0) * pi
 
         self.robotWidth = 80
         self.robotHeight = 40
         self.armLength = 60
         self.handLength = 40
         self.positions = [0, 0]
-        self.velavg_msg = None
+        self.vel_avg_msg = None
         self.vel_msg = None
         self.pos_msg = None
         self.step_msg = None
@@ -257,19 +271,19 @@ class CrawlingRobot:
         else:
             self.robotPos = (20, 0)
 
-    def set_angles(self, armAngle, handAngle):
+    def set_angles(self, arm_angle, hand_angle):
         """
             set the robot's arm and hand angles
             to the passed in values
         """
-        self.armAngle = armAngle
-        self.handAngle = handAngle
+        self.arm_angle = arm_angle
+        self.hand_angle = hand_angle
 
     def get_angles(self):
         """
             returns the pair of (armAngle, handAngle)
         """
-        return self.armAngle, self.handAngle
+        return self.arm_angle, self.hand_angle
 
     def get_robot_position(self):
         """
@@ -287,11 +301,11 @@ class CrawlingRobot:
             raise ValueError('Crawling Robot: Arm Raised too high. Careful!')
         if new_arm_angle < self.minArmAngle:
             raise ValueError('Crawling Robot: Arm Raised too low. Careful!')
-        disp = self.displacement(self.armAngle, self.handAngle,
-                                 new_arm_angle, self.handAngle)
+        disp = self.displacement(self.arm_angle, self.hand_angle,
+                                 new_arm_angle, self.hand_angle)
         cur_x_pos = self.robotPos[0]
         self.robotPos = (cur_x_pos + disp, self.robotPos[1])
-        self.armAngle = new_arm_angle
+        self.arm_angle = new_arm_angle
 
         # Position and Velocity Sign Post
         self.positions.append(self.get_robot_position()[0])
@@ -307,10 +321,10 @@ class CrawlingRobot:
             raise ValueError('Crawling Robot: Hand Raised too high. Careful!')
         if new_hand_angle < self.minHandAngle:
             raise ValueError('Crawling Robot: Hand Raised too low. Careful!')
-        disp = self.displacement(self.armAngle, self.handAngle, self.armAngle, new_hand_angle)
-        curXPos = self.robotPos[0]
-        self.robotPos = (curXPos + disp, self.robotPos[1])
-        self.handAngle = new_hand_angle
+        disp = self.displacement(self.arm_angle, self.hand_angle, self.arm_angle, new_hand_angle)
+        cur_x_pos = self.robotPos[0]
+        self.robotPos = (cur_x_pos + disp, self.robotPos[1])
+        self.hand_angle = new_hand_angle
 
         # Position and Velocity Sign Post
         self.positions.append(self.get_robot_position()[0])
@@ -336,8 +350,8 @@ class CrawlingRobot:
             get the current angle the
             robot body is rotated off the ground
         """
-        arm_cos, arm_sin = self.__get_cos_and_sin(self.armAngle)
-        hand_cos, hand_sin = self.__get_cos_and_sin(self.handAngle)
+        arm_cos, arm_sin = self.__get_cos_and_sin(self.arm_angle)
+        hand_cos, hand_sin = self.__get_cos_and_sin(self.hand_angle)
         x = self.armLength * arm_cos + self.handLength * hand_cos + self.robotWidth
         y = self.armLength * arm_sin + self.handLength * hand_sin + self.robotHeight
         if y < 0:
@@ -370,9 +384,7 @@ class CrawlingRobot:
                 return 0.0
             return -(x - y * (x_old - x) / (y_old - y)) + math.sqrt(x_old * x_old + y_old * y_old)
 
-        raise ValueError('Never Should See This!')
-
-    def draw(self, stepCount, root):
+    def draw(self, step_count, root):
         if self.canvas is None or root is None:
             return
         x1, y1 = self.get_robot_position()
@@ -396,13 +408,13 @@ class CrawlingRobot:
 
         self.canvas.coords(self.robotBody, x1, y1, x2, y2, x4, y4, x3, y3)
 
-        arm_cos, arm_sin = self.__get_cos_and_sin(rotation_angle + self.armAngle)
+        arm_cos, arm_sin = self.__get_cos_and_sin(rotation_angle + self.arm_angle)
         x_arm = x4 + self.armLength * arm_cos
         y_arm = y4 - self.armLength * arm_sin
 
         self.canvas.coords(self.robotArm, x4, y4, x_arm, y_arm)
 
-        hand_cos, hand_sin = self.__get_cos_and_sin(self.handAngle + rotation_angle)
+        hand_cos, hand_sin = self.__get_cos_and_sin(self.hand_angle + rotation_angle)
         x_hand = x_arm + self.handLength * hand_cos
         y_hand = y_arm - self.handLength * hand_sin
 
@@ -415,17 +427,17 @@ class CrawlingRobot:
         vel_msg = '100-step Avg Velocity: %.2f' % self.velAvg
         velocity_msg = 'Velocity: %.2f' % velocity
         position_msg = 'Position: %2.f' % pos
-        step_msg = 'Step: %d' % stepCount
+        step_msg = 'Step: %d' % step_count
         if 'vel_msg' in dir(self):
             self.canvas.delete(self.vel_msg)
             self.canvas.delete(self.pos_msg)
             self.canvas.delete(self.step_msg)
-            self.canvas.delete(self.velavg_msg)
+            self.canvas.delete(self.vel_avg_msg)
 
-        self.velavg_msg = self.canvas.create_text(650, 190, text=vel_msg)
+        self.vel_avg_msg = self.canvas.create_text(650, 190, text=vel_msg)
         self.vel_msg = self.canvas.create_text(450, 190, text=velocity_msg)
         self.pos_msg = self.canvas.create_text(250, 190, text=position_msg)
         self.step_msg = self.canvas.create_text(50, 190, text=step_msg)
 
-        self.lastStep = stepCount
+        self.lastStep = step_count
         root.update()
