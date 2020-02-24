aa5374354c8a3e75b8457a0894534a3c3f918c2f
Leonardo Araujo dos Santos <leonardo_a@apple.com>
2020-02-24 10:45:10 +0000

    [RLB-4] Minor refactor

--- crawler_example/lego/ex_q_learning.py
+++ crawler_example/lego/ex_q_learning.py
@@ -2,7 +2,8 @@
 from crawler_lego_env import CrawlingRobotEnv
 from agent import Q_Agent
 
-num_iterations_train = 4000
+# Number seem to converge in simulation
+num_iterations_train = 400
 # Bigger values decay faster
 e_greedy_decay = 1. / num_iterations_train
 initial_e_greedy_prob = 1.0
@@ -12,11 +13,9 @@ if __name__ == '__main__':
     env = CrawlingRobotEnv()
     current_state = env.reset()
     agent = Q_Agent(env, gamma=0.9, alpha=0.2, e_greedy_prob=initial_e_greedy_prob, e_greedy_decay=e_greedy_decay)
-    # Number seem to converge in simulation
-    num_steps = 400
 
     # Train
-    for steps in range(num_steps):
+    for steps in range(num_iterations_train):
         action = agent.choose_action(current_state)
         next_state, reward, done, info = env.step(action)
         agent.update_q_table(current_state, action, reward, next_state)
