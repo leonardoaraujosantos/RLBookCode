cf69606da41c934cb0c08c2867f0f80410405270
Leonardo Araujo dos Santos <leonardo_a@apple.com>
2020-02-27 18:57:08 +0000

    [RLB-4] Walk forward version ...

--- crawler_example/lego/test_actions.py
+++ crawler_example/lego/test_actions.py
@@ -17,8 +17,9 @@ print('Should display on VisualStudio')
 seedling = int(round(time.time()))
 random.seed(seedling)
 
-# Initialize environemnt
-env = CrawlingRobotEnv(step_angle=30)
+# Initialize environment
+# If we invert the reward during training the robot should change direction
+env = CrawlingRobotEnv(step_angle=30, invert_reward=True)
 current_state = env.reset()
 agent = Q_Agent(env, gamma=0.9, alpha=0.2)
 
@@ -30,12 +31,19 @@ agent = Q_Agent(env, gamma=0.9, alpha=0.2)
 # 4: FEET UP
 # 5: FEET DOWN
 print('Distance:', env.read_sensor())
-#list_actions_backward = [2,5,1,2,5,1,2,5,1,2,5,1,2,5,1]
-# DO FROM HERE......
-list_actions_forward = [1,5,2,4,1,5,2,4]
+#list_actions = [2,5,1,2,5,1,2,5,1,2,5,1,2,5,1]
+
+# Forward
+list_actions = [1, 5, 2, 4, 1, 5, 2, 4, 1, 5, 2, 4, 1, 5, 2, 4, 1, 5, 2, 4, 1, 5, 2, 4, 0, 3]
+
+# Exercise action, check if we have a positive sum of rewards
 sum_reward = 0
-for action in list_actions_forward:
+for action in list_actions:
     next_state, reward, done, info = env.step(action)
-    print('Action:',action , 'Reward:', reward, 'next_state:', next_state)
+    print('Action:', action, 'Reward:', reward, 'next_state:', next_state)
     sum_reward += reward
 print('Sum of rewards:', sum_reward)
+if sum_reward > 0:
+    print('Good')
+else:
+    print('Bad ....')
